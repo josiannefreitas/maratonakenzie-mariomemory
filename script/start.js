55358a2ffa0358754ca4c7aa0e2d627272623cf2
Josianne <anne.jmfreitas@gmail.com>
2022-09-17 11:29:20 -0400

    button edit

--- script/start.js
+++ script/start.js
@@ -1,5 +1,5 @@
 const buttonIniciar = document.getElementById('iniciar')
 
 buttonIniciar.addEventListener('click', () => {
-  location.assign('https://github.com/josiannefreitas/maratonakenzie-mariomemory/blob/master/jogo.html')
+  location.assign('../jogo.html')
 })
