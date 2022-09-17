98dabd4932a074433c81c1c077fa1cfcac1ddaec
Josianne <anne.jmfreitas@gmail.com>
2022-09-17 13:01:58 -0400

    image uptade

--- script/database.js
+++ script/database.js
@@ -2,21 +2,21 @@ const personagens = [
   {
     id: '0',
     name: 'boo',
-    img: '../img/Boo.png'
+    img: './img/Boo.png'
   },
   {
     id: '1',
     name: 'goomba',
-    img: '../img/Goomba.png'
+    img: './img/Goomba.png'
   },
   {
     id: '2',
     name: 'shell',
-    img: '../img/Shell.png'
+    img: './img/Shell.png'
   },
   {
     id: '3',
     name: 'catnip',
-    img: '../img/Piranha.png'
+    img: './img/Piranha.png'
   }
 ]
